c28bdcca872972d5e128c4a4195730d114d8330c
Elban Avdylaj <elbani@gmail.com>
2016-04-28 23:53:58 -0600

    Refactorings on Main class

--- src/com/aca/dbpmla/Main.java
+++ src/com/aca/dbpmla/Main.java
@@ -13,6 +13,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -22,9 +23,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Integer[] initials = new Integer[62];
-        Arrays.fill(initials, 1);
-        List<Integer> integers = Arrays.asList(initials);
+        List<Integer> integers = new LinkedList<>(Collections.nCopies(62, 1));
+        double total = 0.0;
+        int accuratelyPredicted = 0;
 
         InputStream inputStream = System.in;
 
@@ -49,49 +50,37 @@ public class Main {
 
         Predictor perceptronPredictor = new PerceptronPredictor();
 
-        perceptronTable.initializePerceptronTable(initials.length, perceptronTable.getPerceptrons());
+        perceptronTable.initializePerceptronTable(integers.size(), perceptronTable.getPerceptrons());
 
-        double total = 0.0;
-        int accuratePredicted = 0;
+        for (Trace trace : traceList) {
 
-        for(Trace trace : traceList) {
-            if (!trace.getTakenNotBranch().equals("-")) {
+            Long instructionAddress = trace.getInstructionAddress();
 
-                Long instructionAddress = trace.getInstructionAddress();
+            double prediction = perceptronPredictor.predict(instructionAddress % 940, perceptronTable, globalHistoryTable);
 
-                double prediction = perceptronPredictor.predict(instructionAddress % 940, perceptronTable, globalHistoryTable);
-
-                int t=-1;
-                if (trace.getTakenNotBranch().equals("T")){
-                    t = 1;
-                }
-
-                perceptronPredictor.train(instructionAddress % 940, prediction, t, perceptronTable, globalHistoryTable);
-
-                boolean branchOutcome = false;
-                if (trace.getTakenNotBranch().equals("T")) {
-                    globalHistoryTable.getGlobalHistoryTable().remove(1);
-                    globalHistoryTable.getGlobalHistoryTable().add(1);
-                    branchOutcome = true;
-                }
-                else {
-                    globalHistoryTable.getGlobalHistoryTable().remove(1);
-                    globalHistoryTable.getGlobalHistoryTable().add(-1);
-                }
-
-                boolean perceptronPrediction = false;
-                if(prediction>0)
-                    perceptronPrediction = true;
-
-                if(perceptronPrediction == branchOutcome)
-                    accuratePredicted++;
-
-                total = total + 1.0;
-
-
+            int t = -1;
+            if (trace.getTakenNotBranch().equals("T")) {
+                t = 1;
             }
+
+            perceptronPredictor.train(instructionAddress % 940, prediction, t, perceptronTable, globalHistoryTable);
+
+            boolean branchOutcome = false;
+            if (trace.getTakenNotBranch().equals("T")) {
+                globalHistoryTable.getGlobalHistoryTable().remove(1);
+                globalHistoryTable.getGlobalHistoryTable().add(1);
+                branchOutcome = true;
+            } else {
+                globalHistoryTable.getGlobalHistoryTable().remove(1);
+                globalHistoryTable.getGlobalHistoryTable().add(-1);
+            }
+
+            if (prediction > 0 == branchOutcome)
+                accuratelyPredicted++;
+
+            total = total + 1.0;
         }
-        System.out.println("Accuracy: " + accuratePredicted/total);
+        System.out.println("Accuracy: " + accuratelyPredicted / total);
 
     }
 }
