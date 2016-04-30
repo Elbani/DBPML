f7cd04b33a4042996565e0513469f4e289c1f6b7
Elban Avdylaj <elbani@gmail.com>
2016-04-30 17:06:52 -0600

    Refactorings and performance improvements on perceptron predictor

---
+++ src/edu/aca/dbpmla/perceptron/Main.java
@@ -0,0 +1,13 @@
+package edu.aca.dbpmla.perceptron;
+
+import edu.aca.dbpmla.perceptron.predictorsImpl.PerceptronPredictor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PerceptronPredictor perceptronPredictor = new PerceptronPredictor();
+        perceptronPredictor.runPerceptron();
+
+    }
+}
