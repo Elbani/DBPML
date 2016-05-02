28f13dfd7ea0b27c256d698318ec39466931e577
Taulant <ymeritaulant@gmail.com>
2016-05-02 05:18:52 -0600

    Implementation of Three Versions of Naive Bayesian Predictors

--- src/main/java/edu/aca/dbpmla/naivebayesian/entity/NaiveBayesianStructure.java
+++ src/main/java/edu/aca/dbpmla/naivebayesian/entity/NaiveBayesianStructure.java
@@ -1,23 +1,28 @@
 package edu.aca.dbpmla.naivebayesian.entity;
 
+import edu.aca.dbpmla.naivebayesian.predictor.NaiveBayesianImplementation;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 /**
  * Created by Taulant on 5/1/2016.
  */
-public class NaiveBayesianStructure {
+public class NaiveBayesianStructure extends NaiveBayesianImplementation {
     private List<Integer> historyTable;
     private Map<Integer, Integer> historyCountersP0, historyCountersP1;
     private int p1;
     private int historyBitLength;
     private int bitCounterLength;
 
-    public NaiveBayesianStructure(List<Integer> historyTable, Map<Integer, Integer> historyCountersP0, Map<Integer, Integer> historyCountersP1, int p1, int historyBitLength, int bitCounterLength) {
-        this.historyTable = historyTable;
-        this.historyCountersP0 = historyCountersP0;
-        this.historyCountersP1 = historyCountersP1;
-        this.p1 = p1;
+    public NaiveBayesianStructure(int historyBitLength, int bitCounterLength) {
+        this.historyTable = new ArrayList<>(Collections.nCopies(historyBitLength, 0));
+        this.historyCountersP0 = generateCounters(historyTable.size());
+        this.historyCountersP1 = generateCounters(historyTable.size());
+
+        this.p1 = 0;
         this.historyBitLength = historyBitLength;
         this.bitCounterLength = bitCounterLength;
     }
